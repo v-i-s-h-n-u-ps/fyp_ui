fd26ec019e46f965c9bc02ec20ee06d825d3ef3b
vishnu.sivan <vishnu.sivan@whitehatjr.com>
2021-05-02 15:34:02 +0530

    back navigation

--- src/components/common/navigateTo/index.js
+++ src/components/common/navigateTo/index.js
@@ -1,22 +1,43 @@
 import React from "react";
-import Link from "next/link";
+import { useRouter } from "next/router";
 
 import s from "./index.module.scss";
+import Ripple from "../../_hoc/Ripple";
 
 const NavigateTo = (props) => {
-    const { title, link } = props;
+
+    const { title, link, children, style = "space-between" } = props;
+
+    const Router = useRouter();
+
+    const back = () => {
+        !!link
+            ? typeof link === 'function'
+                ? link()
+                : Router.push(link)
+            : Router.back()
+    }
 
     return (
-        <div className={`${s.navigateContainer}`}>
-            <Link href={link} prefetch={false}>
-                <a href={link} className={`${s.navigateTo} gtm-back-home`}>
-                    <i className={`${s.icon} icon-arrow_left`} />
-                    <div className={`${s.title}`}>{title}</div>
-                </a>
-            </Link>
-        </div>
+        <>
+            <div className={`${s.navigateContainer} navigateContainer`} >
+                <div className={`${s.navigateTo}`} onClick={back}>
+                    <Ripple >
+                        <div className={`${s.navigate}`}>
+                            <i className={`${s.icon} icon-arrow_left`} />
+                            <div className={`${s.title}`}>{title}</div>
+                        </div>
+                    </Ripple>
+                </div>
+                {children}
+            </div>
+            <style jsx>{`
+                .navigateContainer {
+                    justify-content: ${style}
+                }
+            `}</style>
+        </>
     )
 }
 
 export default NavigateTo;
-
