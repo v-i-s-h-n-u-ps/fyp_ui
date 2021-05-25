007c488563b341c3401d0b3266c46ba816755f10
vishnu.sivan <vishnu.sivan@whitehatjr.com>
2021-05-25 11:02:44 +0530

    fix

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -4,26 +4,27 @@ import s from './Tab.module.scss';
 import Tab from './Tab';
 
 const Tabs = ({ data, activeTabValue, callback }) => {
-    const changeTabs = (item) => {
-        callback({ tab: item })
-    }
+  const changeTabs = (item) => {
+    callback({ tab: item })
+  }
 
-    return (
-        <div className={s.tabContainer}>
-            <ol className={`${s.tabList}`}>
-                {data.map((item) => {
-                    return (
-                        <Tab
-                            activeTab={activeTabValue}
-                            key={item.value}
-                            data={item}
-                            handleClick={(item) => { changeTabs(item) }}
-                        />
-                    );
-                })
-                }
-            </ol>
-        </div>
-    );
+  return (
+    <div className={s.tabContainer}>
+      <ol className={`${s.tabList}`}>
+        {data.map((item) => {
+          return (
+            <Tab
+              activeTab={activeTabValue}
+              key={item.value}
+              data={item}
+              handleClick={(item) => { changeTabs(item) }}
+            />
+          );
+        })
+        }
+      </ol>
+    </div>
+  );
 };
+
 export default Tabs;
