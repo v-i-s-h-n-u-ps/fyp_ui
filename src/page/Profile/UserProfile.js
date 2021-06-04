60aaaac01ffb3cb7512f63581bbd8407e13c8543
vishnu.sivan <vishnu.sivan@whitehatjr.com>
2021-06-05 04:05:34 +0530

    fix

--- src/page/Profile/UserProfile.js
+++ src/page/Profile/UserProfile.js
@@ -32,9 +32,9 @@ const Profile = props => {
   const setTab = (param) => {
     const tab = _get(param, 'tab.value', 'upload-file');
     setActiveTabValue(tab);
-    router.push({ 
-      pathname: USER_PROFILE, 
-      query: { ...router.query, tab } 
+    router.push({
+      pathname: USER_PROFILE,
+      query: { ...router.query, tab }
     }, undefined, { shallow: true });
   }
 
@@ -64,7 +64,10 @@ const Profile = props => {
       selectUserInfo={selectUserInfo}
       showActions={false}
     />,
-    'details': <Details selectStudentInfo={_get(selectUserProfile, 'student', {})} showEdit={false} />,
+    'details': <Details
+      selectStudentInfo={_get(selectUserProfile, 'student', {})}
+      showEdit={false}
+    />,
   }
 
   return (
@@ -78,14 +81,47 @@ const Profile = props => {
       </div>
       <div className={s.account}>
         <div className={s.name}>
-          <div className={s.nameArea}>
+          <div className={`${s.nameArea} ${s.noPadding}`}>
             {_get(selectUserProfile, "name")}
           </div>
         </div>
         <div className={s.email}>
           {_get(selectUserProfile, "email")}
         </div>
-        <br />
+        <div className={s.aboutMe}>
+          <label>About</label>
+          <p>{_get(selectUserProfile, "student.about")}</p>
+          <div className={s.social}>
+            {_get(selectUserProfile, "student.gmail") &&
+              <a href={`mailto:${_get(selectUserProfile, "student.gmail")}`}>
+                <div className={s.icon}>
+                  <i className="icon-mail_circle" />
+                </div>
+              </a>
+            }
+            {_get(selectUserProfile, "student.linkedIn") &&
+              <a href={_get(selectUserProfile, "student.linkedIn")} target="_blank">
+                <div className={s.icon}>
+                  <i className="icon-linkedin" />
+                </div>
+              </a>
+            }
+            {_get(selectUserProfile, "student.twitter") &&
+              <a href={_get(selectUserProfile, "student.twitter")} target="_blank">
+                <div className={s.icon}>
+                  <i className="icon-twitter" />
+                </div>
+              </a>
+            }
+            {_get(selectUserProfile, "student.facebook") &&
+              <a href={_get(selectUserProfile, "student.facebook")} target="_blank">
+                <div className={s.icon}>
+                  <i className="icon-facebook" />
+                </div>
+              </a>
+            }
+          </div>
+        </div>
         <Tabs data={tabItems} activeTabValue={activeTabValue} callback={setTab} />
         {components[activeTabValue]}
       </div>
